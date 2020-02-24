45e09888b0117afc5b6d023fcd06fc7ee1c4c386
costolo <ben.costolo@gmail.com>
2020-02-24 17:50:12 -0500

    add shell-db to makefile and correct error in manage.py

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ test:
 	docker-compose exec users pytest "project/tests"
 shell:
 	docker-compose exec users flask shell
+shell-db:
+	docker-compose exec users-db psql -U postgres
--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 # manage.py
 
 import sys
-
 from flask.cli import FlaskGroup
+
 from project import create_app, db
+from project.api.models import User
 
 app = create_app()
-cli = FlaskGroup(app)
+cli = FlaskGroup(create_app=create_app)
 
 @cli.command('recreate_db')
 def recreate_db():
